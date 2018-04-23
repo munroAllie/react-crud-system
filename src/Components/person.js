40b3596b5caf855d196b4c79e855d77f5ef77e2c
Allie Munro <allie.munro@smartsti.com>
2018-04-23 09:04:51 -0300

    fixes save/edit button hitzone

--- src/Components/person.js
+++ src/Components/person.js
@@ -12,36 +12,36 @@ const Person = (props) => {
     <tr>
       <th scope="row">{props.id}</th>
       <td>
-      <input 
-        type="text" 
+      <input
+        type="text"
         data-index={props.index}
         data-key={props.id}
         className={`form-control ${editing === false ? 'disabled' : null}`}
-        id="firstName" 
-        placeholder="enter first name" 
-        value={props.firstName} 
-        onChange={props.dataChange}  
+        id="firstName"
+        placeholder="enter first name"
+        value={props.firstName}
+        onChange={props.dataChange}
       />
       </td>
       <td>
-      <input 
-        type="text" 
+      <input
+        type="text"
         data-index={props.index}
         data-key={props.id}
-        className={`form-control ${editing === false ? 'disabled' : null}`} 
+        className={`form-control ${editing === false ? 'disabled' : null}`}
         id="lastName"
-        placeholder="enter last name" 
-        value={props.lastName} 
-        onChange={props.dataChange} 
+        placeholder="enter last name"
+        value={props.lastName}
+        onChange={props.dataChange}
       />
       </td>
       <td>
       { editing === false ?
-        <div className="btn btn-primary">
-          <i className="fas fa-pencil-alt" onClick={props.editContact} />
+        <div className="btn btn-primary" onClick={props.editContact}>
+          <i className="fas fa-pencil-alt"  />
         </div> :
-      <div className="btn btn-success">
-        <i className="far fa-save" onClick={props.saveContact}/>
+      <div className="btn btn-success" onClick={props.saveContact}>
+        <i className="far fa-save" />
       </div> }
         <div className="btn btn-danger" onClick={props.deleteContact}>
           <i className="fas fa-trash-alt" />
